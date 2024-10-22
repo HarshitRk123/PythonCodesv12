ac621d3bf9ddc5d616c15f237b76a4878518ded9
Harshit <harsshitrk0120@gmail.com>
2024-10-22 09:55:19 -0700

    Add files via upload

---
+++ que19.py
@@ -0,0 +1,5 @@
+def welcome_user():
+    first_name = input("Please enter your first name: ")
+    last_name = input("Please enter your last name: ")
+    print(f"Hello {first_name} {last_name}, welcome to Python!")
+welcome_user()
